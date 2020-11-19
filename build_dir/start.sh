389fd0080f2db40e8db598941f3f276004813f26
ericwang2006 <ericwang2006@gmail.com>
2020-11-19 13:59:35 +0000

    fix listen port info

--- build_dir/start.sh
+++ build_dir/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 foundport=0
+last=`date +%s`
 while true;do
     num=`ps fax | grep '/ttnode' | egrep -v 'grep|echo|rpm|moni|guard' | wc -l`;
     if [ $num -lt 1 ];then
-        d=`date '+%F %T'`;
+        d=`date '+%F %T'`
         echo "[$d] ttnode进程不存在,启动ttnode"
         /usr/node/ttnode -p /mnts
         /usr/node/qr.sh
@@ -18,14 +19,19 @@ while true;do
     fi
 
     if [ $foundport -eq 0 ]; then
-        netstat -tnlp|grep -v '127\|Proto\|Active'|awk '{print $4}'|sed 's/0.0.0.0://' > /usr/node/port.txt
+        netstat -nlp|grep "$(ps fax | grep '/ttnode' | egrep -v 'grep|echo|rpm|moni|guard'|awk '{print $1}')/"|grep -v '127.0.0.1\|17331'|awk '{sub(/0.0.0.0:/,""); print $1,$4}'|sort -k 2n -k 1 > /usr/node/port.txt
         len=`sed -n '$=' /usr/node/port.txt`
-        if [[ $len -gt 2 ]]; then
+        if [[ $len -gt 4 ]]; then
             echo "==========================================================================="
-            echo "如果UPNP失效，请在路由器上对下面3个端口做转发"
+            d=`date '+%F %T'`
+            echo "[$d] 如果UPNP失效，请在路由器上对下列端口做转发"
             cat /usr/node/port.txt
             echo "==========================================================================="
             foundport=1
+            last=`date +%s`
+        else
+            d=`date '+%F %T'`
+            echo "[$d] 正在获取端口信息..."
         fi
     fi
     
@@ -33,5 +39,10 @@ while true;do
         sleep 20
     else
         sleep 60
+        now=`date +%s`
+        diff=$(($now-$last))
+        if [[ $diff -gt 43200 ]]; then #12 hour
+            foundport=0
+        fi
     fi
 done
