b33db267afa36d9f520eb46e4195de5de454b606
ericwang2006 <erciwang2006@gmail.com>
2020-12-05 16:18:28 +0000

    purge cdn cache

--- buildx.sh
+++ buildx.sh
@@ -1,1 +1,2 @@
+curl https://purge.jsdelivr.net/gh/ericwang2006/docker_ttnode/aarch64/ttnode
 docker buildx build -t ericwang2006/ttnode --platform=linux/arm64 -o type=docker ./build_dir
--- buildx_v7.sh
+++ buildx_v7.sh
@@ -1,1 +1,2 @@
+curl https://purge.jsdelivr.net/gh/ericwang2006/docker_ttnode/armv7l/ttnode
 docker buildx build -t ericwang2006/ttnode:armv7 --platform=linux/arm/v7 -o type=docker ./build_dir
