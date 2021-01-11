8c51310e5f5635bac089cf6aea6863b577b5a859
ericwang2006 <erciwang2006@gmail.com>
2021-01-11 22:54:23 +0800

    add env DISABLE_ATUO_TASK

--- build_dir/start.sh
+++ build_dir/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-service cron start
-crontab /usr/node/crontab_list.sh
+if [ $DISABLE_ATUO_TASK != "1" ]; then
+	service cron start
+	crontab /usr/node/crontab_list.sh
+fi
 
 foundport=0
 last=$(date +%s)
