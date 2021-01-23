c542a55deca95bed566c44c7ac665202a1d0b08a
ericwang2006 <erciwang2006@gmail.com>
2021-01-23 11:13:11 +0800

    v181

--- build_dir/init_arch.sh
+++ build_dir/init_arch.sh
@@ -10,14 +10,15 @@ x86_64)
 
 	qemu="/usr/bin/qemu-arm-static"
 	curl -L -o $qemu https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-2/qemu-aarch64-static && chmod +x $qemu
-	# curl -k -o /usr/node/ttnode https://cdn.jsdelivr.net/gh/ericwang2006/docker_ttnode/$(uname -m)/ttnode
-	curl -L -k -o /usr/node/ttnode http://o7coj731m.bkt.clouddn.com/tiantang/arm64/$ttnode_filename && chmod +x /usr/node/ttnode
+	# curl -L -k -o /usr/node/ttnode https://cdn.jsdelivr.net/gh/ericwang2006/docker_ttnode/$(uname -m)/ttnode
+	# curl -L -k -o /usr/node/ttnode http://o7coj731m.bkt.clouddn.com/tiantang/arm64/$ttnode_filename
+	curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/master/aarch64/ttnode && chmod +x /usr/node/ttnode
 	;;
 aarch64)
-	curl -L -k -o /usr/node/ttnode http://o7coj731m.bkt.clouddn.com/tiantang/arm64/$ttnode_filename && chmod +x /usr/node/ttnode
+	curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/master/$(uname -m)/ttnode && chmod +x /usr/node/ttnode
 	;;
 armv7l)
-	curl -L -k -o /usr/node/ttnode http://o7coj731m.bkt.clouddn.com/tiantang/arm32/$ttnode_filename && chmod +x /usr/node/ttnode
+	curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/master/$(uname -m)/ttnode && chmod +x /usr/node/ttnode
 	;;
 *)
 	echo "不支持的处理器平台!!!"
