790713ef169734415c8ee73b6780f815828421e5
ericwang2006 <erciwang2006@gmail.com>
2021-02-14 15:59:57 +0800

    get_offline_file.sh

--- get_offline_file.sh
+++ get_offline_file.sh
@@ -6,11 +6,12 @@ function save_img() {
 	m=$(echo $manifest | jq -r ".[] | select(.Descriptor.platform.architecture==\"$1\")|.SchemaV2Manifest.config.digest")
 	m=${m:7:12}
 	docker tag $m $image_name:$tag
-	docker save $image_name:$tag | gzip >ttnode_"$2"_latest.tar.gz
+	docker save $image_name:$tag | gzip >"$prefix"_"$2"_"$tag".tar.gz
 }
 
 image_name="ericwang2006/ttnode"
 tag="latest"
+prefix="ttnode"
 manifest=$(docker manifest inspect -v $image_name:$tag)
 save_img "amd64" "amd64"
 save_img "arm64" "arm64"
