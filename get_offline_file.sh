26961266dc7e6a8c661f8ff9b920508c23e84406
ericwang2006 <erciwang2006@gmail.com>
2021-02-14 15:56:22 +0800

    get_offline_file.sh

--- get_offline_file.sh
+++ get_offline_file.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 function save_img() {
-	v=$(cat /tmp/tt.json | jq -r ".[] | select(.Descriptor.platform.architecture==\"$1\")|.Descriptor.digest")
-	docker pull ericwang2006/ttnode:latest@$v
+	v=$(echo $manifest | jq -r ".[] | select(.Descriptor.platform.architecture==\"$1\")|.Descriptor.digest")
+	docker pull $image_name:$tag@$v
 
-	m=$(cat /tmp/tt.json | jq -r ".[] | select(.Descriptor.platform.architecture==\"$1\")|.SchemaV2Manifest.config.digest")
+	m=$(echo $manifest | jq -r ".[] | select(.Descriptor.platform.architecture==\"$1\")|.SchemaV2Manifest.config.digest")
 	m=${m:7:12}
-	docker tag "$m" ericwang2006/ttnode:latest
-	docker save ericwang2006/ttnode:latest | gzip >ttnode_"$2"_latest.tar.gz
+	docker tag $m $image_name:$tag
+	docker save $image_name:$tag | gzip >ttnode_"$2"_latest.tar.gz
 }
 
-docker manifest inspect -v ericwang2006/ttnode >/tmp/tt.json
-rm -f ttnode*latest.tar.gz
+image_name="ericwang2006/ttnode"
+tag="latest"
+manifest=$(docker manifest inspect -v $image_name:$tag)
 save_img "amd64" "amd64"
 save_img "arm64" "arm64"
 save_img "arm" "arm32"
-rm /tmp/tt.json
