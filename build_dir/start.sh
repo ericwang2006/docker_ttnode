33c9ab3a3a08d32e02fe4bcab82adb7104abfc04
ericwang2006 <erciwang2006@gmail.com>
2021-02-15 10:34:22 +0800

    ensure openssl install

--- build_dir/start.sh
+++ build_dir/start.sh
@@ -81,7 +81,7 @@ while true; do
 	fi
 
 	if [ $foundport -eq 0 ]; then
-		netstat -nlp | grep "$(ps fax | grep '/ttnode' | egrep -v 'grep|echo|rpm|moni|guard' | awk '{print $1}')/" | grep -v '127.0.0.1\|17331' | awk '{sub(/0.0.0.0:/,""); print $1,$4}' | sort -k 2n -k 1 >/usr/node/port.txt
+		netstat -nlp | grep "$(ps fax | grep '/ttnode' | egrep -v 'grep|echo|rpm|moni|guard' | awk '{print $1}')/" | grep -v '127.0.0.1\|17331\|unix ACC' | awk '{sub(/0.0.0.0:/,""); print $1,$4}' | sort -k 2n -k 1 >/usr/node/port.txt
 		len=$(sed -n '$=' /usr/node/port.txt)
 		if [[ $len -gt 4 ]]; then
 			echo "==========================================================================="
