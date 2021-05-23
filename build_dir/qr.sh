a18d511b1f88ea6c13c293f3ff92905cd9eaa6ba
ericwang2006 <erciwang2006@gmail.com>
2021-05-23 14:38:03 +0800

    v207

--- build_dir/qr.sh
+++ build_dir/qr.sh
@@ -26,6 +26,9 @@ else
 	echo $uid >/usr/node/uid.txt
 	echo "==========================================================================="
 	echo "如果觉得还有点用，麻烦用一下我的邀请码631441，有加成卡15张，我也有推广收入"
+	echo "视频教程:"
+	echo "https://www.youtube.com/playlist?list=PLTes8sqjACw1MY4Pq_QgBLN-I4cEE-wcO"
+	echo "https://www.bilibili.com/video/BV1G64y117Na"
 	# echo -e "甜糖客户端如果不能扫描到您的设备，请在浏览器访问\nhttps://ericwang2006.github.io/docker_ttnode/qrgen.htm?str=$uid\n获取二维码并用甜糖客户端扫描添加(纯js实现,不会发生数据泄露)"
 	# dev=$(find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\n' | head -n 1)
 	dev=$(route | grep defaul | awk '{print $8}' | head -n 1)
