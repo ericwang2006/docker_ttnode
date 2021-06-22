fe446a1803d03f9f34476987de1167511ce193de
ericwang2006 <erciwang2006@gmail.com>
2021-06-22 09:27:30 +0800

    armbian version

--- build_dir/init_arch.sh
+++ build_dir/init_arch.sh
@@ -11,7 +11,7 @@ x86_64)
 		curl -L -o $qemu https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-2/qemu-arm-static && chmod +x $qemu
 		# curl -L -k -o /usr/node/ttnode https://cdn.jsdelivr.net/gh/ericwang2006/docker_ttnode/$(uname -m)/ttnode
 		# curl -L -k -o /usr/node/ttnode http://o7coj731m.bkt.clouddn.com/tiantang/arm64/$ttnode_filename
-		curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/master/armv7l/ttnode && chmod +x /usr/node/ttnode
+		curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/armbian/armv7l/ttnode && chmod +x /usr/node/ttnode
 	elif [ $EMULATION_PLATFORM = "arm64" ]; then
 		dpkg --add-architecture arm64
 		apt-get update
@@ -20,17 +20,17 @@ x86_64)
 
 		qemu="/usr/bin/qemu-aarch64-static"
 		curl -L -o $qemu https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-2/qemu-aarch64-static && chmod +x $qemu
-		curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/master/aarch64/ttnode && chmod +x /usr/node/ttnode
+		curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/armbian/aarch64/ttnode && chmod +x /usr/node/ttnode
 	else
 		echo "$EMULATION_PLATFORM unsupported param!"
 		exit 1
 	fi
 	;;
 aarch64)
-	curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/master/aarch64/ttnode && chmod +x /usr/node/ttnode
+	curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/armbian/aarch64/ttnode && chmod +x /usr/node/ttnode
 	;;
 armv7l)
-	curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/master/armv7l/ttnode && chmod +x /usr/node/ttnode
+	curl -L -k -o /usr/node/ttnode https://github.com/ericwang2006/docker_ttnode/raw/armbian/armv7l/ttnode && chmod +x /usr/node/ttnode
 	;;
 *)
 	echo "unsupported CPU architecture!"
